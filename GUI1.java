352c2c8869d8f23e64c092a05fa2cad6f93d1689
Ganesh Gaikwad <103919754+ganeshgaikwad1112@users.noreply.github.com>
2023-08-14 20:51:36 +0530

    Add files via upload

---
+++ GUI1.java
@@ -0,0 +1,16 @@
+// GUI
+
+import java.awt.*;
+
+
+
+class GUI1
+{
+    public static void main(String a[])
+    {
+        Frame fobj = new Frame("Marvellous");
+        fobj.setSize(500,500);
+
+        fobj.setVisible(true);
+    }
+}
---
+++ GUI6.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class MarvellousFrame
+{
+    public MarvellousFrame(String title)
+    {
+        Frame fobj = new Frame(title);
+        fobj.setSize(500,500);
+        fobj.setVisible(true);
+
+        Button bobj = new Button("ok");
+        fobj.add(bobj);
+
+        fobj.addWindowListener(new MarvellousListener());
+    }
+}
+
+class MarvellousListener extends WindowAdapter
+{
+    
+    public void windowClosing(WindowEvent obj)
+    {
+        System.exit(0);
+    }
+   
+}
+
+class GUI6
+{
+    public static void main(String arg[])
+    {
+        MarvellousFrame mobj = new MarvellousFrame("Marvellous PPA41");
+
+    }
+}
